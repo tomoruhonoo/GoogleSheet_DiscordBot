e13ae94d69807256d3d3d400327fa53267dbe7f7
tomoruhonoo <111542906+tomoruhonoo@users.noreply.github.com>
2022-09-28 06:49:30 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -153,7 +153,7 @@ async def on_message(message):
     # react to user-specific message with emoji reaction
     if str(message.author) == '炎炎#4468':
       emoji = await ctx.guild.fetch_emoji(982031160517480540)
-        await message.add_reaction(emoji)
+        await ctx.message.add_reaction(emoji)
         # if you want to use custom server-specific emoji, replace '<:emoji_name:>' with '<:emoji_name:emoji_id_number>'
     
     
