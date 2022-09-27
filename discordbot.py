a657ed82d0305926bd8a7278311ccd09a146d826
tomoruhonoo <111542906+tomoruhonoo@users.noreply.github.com>
2022-09-28 04:29:10 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -151,7 +151,7 @@ async def on_message(message):
         
     # react to user-specific message with emoji reaction
     if str(message.author.name) == '炎炎#4468':
-        await message.add_reaction('<:emoji_name::snake:>')
+        await message.add_reaction('')
         # if you want to use custom server-specific emoji, replace '<:emoji_name:>' with '<:emoji_name:emoji_id_number>'
     
     
