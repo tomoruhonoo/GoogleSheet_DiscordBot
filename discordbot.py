c1adfe8b99a6330c22546f96cd70ff1525aacbde
tomoruhonoo <111542906+tomoruhonoo@users.noreply.github.com>
2022-09-28 07:53:53 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -157,7 +157,7 @@ async def on_message(message):
     if str(message.author) == 'Penguin-quack#2748':
         await message.add_reaction('<:emoji_name:982031160517480540>')
         # if you want to use custom server-specific emoji, replace '<:emoji_name:>' with '<:emoji_name:emoji_id_number>'
-    if str(message.author) == '炎炎#4468':
+    if str(message.author) == '真宮悠楒#9869':
         await message.add_reaction('<:emoji_name:988774529461542912>')
     
     
