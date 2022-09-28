87e8c326e4d45f4d489549e2ce69045df37df106
tomoruhonoo <111542906+tomoruhonoo@users.noreply.github.com>
2022-09-28 21:43:18 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -149,7 +149,7 @@ async def on_message(message):
  #        await message.add_reaction('<:emoji_name:982031160517480540>')
  # == '<@990641822722318376>':
         
-    if str(message.author.role) == None:
+    if str(guild.role) == None:
         await message.add_reaction('<:emoji_name:982031160517480540>')
 
 
