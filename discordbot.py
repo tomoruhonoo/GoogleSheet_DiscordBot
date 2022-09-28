fb65f801df5cbed386208b36f0e8fbfcd397e72c
tomoruhonoo <111542906+tomoruhonoo@users.noreply.github.com>
2022-09-28 22:43:16 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -149,7 +149,7 @@ async def on_message(message):
  #        await message.add_reaction('<:emoji_name:982031160517480540>')
  # == '<@990641822722318376>':
         
-    if str(message.author) == '<user_511896662407512065>':
+    if str(user_id) == '<511896662407512065>':
         await message.add_reaction('<:emoji_name:982031160517480540>')
 
 
